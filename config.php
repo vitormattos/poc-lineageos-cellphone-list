7c6f78ef411b95f65f716d674cf46a557f2a46a3
Vitor Mattos <vitor@php.rio>
2020-01-25 15:00:23 -0300

    release pode vir ano, ano, mês, ano mês e edia e também sem release, também tem o caso de vir release de mais de um modelo. Implementado lógica para tratar estes casos

--- config.php
+++ config.php
@@ -14,11 +14,17 @@ return [
                 foreach (glob(__DIR__.'/lineage_wiki/_data/devices/*.yml') as $filename) {
                     $service = Yaml::parseFile($filename)+[];
                     if (is_numeric($service['release']) && $service['release'] > 10000) {
-                        $service['orderBy'] = date('Y-m-d', $service['release']);
+                        $service['release'] = $service['orderBy'] = date('Y-m-d', $service['release']);
                     } elseif (is_array($service['release'])) {
-                        foreach ($service['release'][0] as $release) {
-                            if (is_numeric($release)) {
+                        foreach ($service['release'] as $key => $release) {
+                            if (is_numeric($release) && $release > 10000) {
                                 $service['orderBy'] = date('Y-m-d', $release);
+                            } else if (is_array($release)) {
+                                foreach($release as $model => $date) {
+                                    if (is_numeric($date) && $date > 10000) {
+                                        $service['orderBy'] = $service['release'][$key][$model] = date('Y-m-d', $date);
+                                    }
+                                }
                             } else {
                                 $service['orderBy'] = $release;
                             }
