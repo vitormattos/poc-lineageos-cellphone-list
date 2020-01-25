54a264a6423196fb9762d4c5b106abdcceaf5730
Vitor Mattos <vitor@php.rio>
2020-01-25 02:54:33 -0300

    Evitando warning

--- source/_partials/battery.blade.php
+++ source/_partials/battery.blade.php
@@ -6,10 +6,12 @@
 @else
     Non-removable
 @endif &nbsp;
-@if ($battery_data['tech'])
+@if (!empty($battery_data['tech']))
     {{ $battery_data['tech'] }}&nbsp;
 @endif
-{{ $battery_data['capacity'] }} mAh
-@if ($battery_data['fastcharge'])
+@if (!empty($battery_data['capacity']))
+    {{ $battery_data['capacity'] }} mAh
+@endif
+@if (!empty($battery_data['fastcharge']))
     &nbsp;with fast charging
 @endif
