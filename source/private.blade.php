a3323e62eb5739a0cd9f683806e3f46e6c7e433a
Vitor Mattos <vitor@php.rio>
2020-01-26 20:49:37 -0300

    Remoção de json de response, não necessário

---
+++ source/private.blade.php
@@ -0,0 +1,13 @@
+---
+title: Login
+---
+@extends('_layouts.master')
+@section('body')
+<section class="section-content padding-y">
+    <div class="card mx-auto" style="max-width:520px; margin-top:40px;">
+        <div class="card-body">
+            <h4 class="card-title mb-4">Welcome!</h4>
+        </div>
+    </div>
+</section>
+@endsection
